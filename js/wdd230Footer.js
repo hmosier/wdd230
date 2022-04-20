4882de1ef426724f6eb4da8213c1f88b0046a4c8
hmosier <hannah89mosier@byui.edu>
2022-04-20 19:09:20 -0400

    last modified change commit

--- js/wdd230Footer.js
+++ js/wdd230Footer.js
@@ -1,5 +1,5 @@
 
-const currentDate = new Date();
+const modified = new Date(document.lastModified);
 
 const year = new Date();
 
@@ -7,4 +7,4 @@ const copyright = "\u00A9";
 
 document.getElementById("copyright").innerHTML = `${copyright} ${year.getFullYear()} .:|:. Hannah Mosier .:|:. Ohio`;
 
-document.getElementById("updated").innerHTML = `Last Updated: ${currentDate}`;
+document.getElementById("updated").innerHTML = `Last Updated: ${modified}`;
