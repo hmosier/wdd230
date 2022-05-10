835d3116c2633d2716699c64f2566d06ab74f783
hmosier <hannah89mosier@byui.edu>
2022-05-10 11:20:34 -0400

    a little more styling commit

--- lessons/chamber/js/c-mound.js
+++ lessons/chamber/js/c-mound.js
@@ -5,3 +5,13 @@ function toggleMenu() {
 
 const x = document.getElementById("hamburger")
 x.onclick = toggleMenu;
+
+const datefieldUK = document.querySelector(".date");
+
+const now = new Date();
+
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
