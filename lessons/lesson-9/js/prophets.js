ba00593d6e12ab5d1cfdbca490cee6c17140a615
hmosier <hannah89mosier@byui.edu>
2022-06-16 15:13:34 -0400

    more javascript

--- lessons/lesson-9/js/prophets.js
+++ lessons/lesson-9/js/prophets.js
@@ -1,33 +1,6 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 const cards = document.querySelector('.cards');
 
-function output (prophets) {
-    prophets.forEach(
-        prophet => {
-            let card = document.createElement("section")
-
-            let name = document.createElement("h2")
-            name.textContent = `${prophet.name} ${prophet.lastname}`;
-
-            let dob = document.createElement("p")
-            dob.textContent = `Date of Birth: ${prophet.birthdate}`;
-
-            let birthplace = document.createElement("p")
-            birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
-
-            let portrait = document.createElement("img")
-            portrait.setAttribute = (src, prophet.imageurl);
-            portrait.setAttribute = (alt, `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order} Latter-day President`);
-            portrait.setAttribute = (loading, lazy);
-
-            card.appendChild(name);
-            card.appendChild(dob);
-            card.appendChild(order);
-            card.appendChild(portrait);
-        }
-    );
-}
-
 async function getProphets() {
     let response = await fetch(requestURL);
     if (response.ok) {
@@ -39,3 +12,27 @@ async function getProphets() {
 }
 
 getProphets();
+
+function output (data) {
+    data.prophets.forEach(prophet => {
+            let card = document.createElement("section")
+            let name = document.createElement("h2")
+            let dob = document.createElement("p")
+            let birthplace = document.createElement("p")
+            let portrait = document.createElement("img")
+
+            name.textContent = `${prophet.name} ${prophet.lastname}`;
+            dob.textContent = `Date of Birth: ${prophet.birthdate}`;
+            birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+            portrait.setAttribute = ("src", prophet.imageurl);
+            portrait.setAttribute = ("alt", `Portrait of ${name} - ${prophet.order}th Latter-day President`);
+            portrait.setAttribute = ("loading", "lazy");
+
+            card.appendChild(name);
+            card.appendChild(dob);
+            card.appendChild(birthplace);
+            card.appendChild(portrait);
+        }
+    );
+}
