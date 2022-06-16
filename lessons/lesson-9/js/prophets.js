d3a1ab20236f6b32fd3bb380f4446bb7c6b9f97a
hmosier <hannah89mosier@byui.edu>
2022-06-16 15:06:21 -0400

    javascript

--- lessons/lesson-9/js/prophets.js
+++ lessons/lesson-9/js/prophets.js
@@ -1,25 +1,23 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 const cards = document.querySelector('.cards');
 
-let prophetsList = [];
-
-const output = (prophets) => {
+function output (prophets) {
     prophets.forEach(
         prophet => {
             let card = document.createElement("section")
 
             let name = document.createElement("h2")
-            name.textContent = `${prophet[i].name} ${prophet[i].lastname}`;
+            name.textContent = `${prophet.name} ${prophet.lastname}`;
 
             let dob = document.createElement("p")
-            dob.textContent = `Date of Birth: ${prophet[i].birthdate}`;
+            dob.textContent = `Date of Birth: ${prophet.birthdate}`;
 
             let birthplace = document.createElement("p")
-            birthplace.textContent = `Place of Birth: ${prophet[i].birthplace}`;
+            birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
 
             let portrait = document.createElement("img")
-            portrait.setAttribute = (src, prophet[i].imageurl);
-            portrait.setAttribute = (alt, `Portrait of ${prophet.name} ${prophet[i].lastname} - ${prophet[i].order} Latter-day President`);
+            portrait.setAttribute = (src, prophet.imageurl);
+            portrait.setAttribute = (alt, `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order} Latter-day President`);
             portrait.setAttribute = (loading, lazy);
 
             card.appendChild(name);
@@ -32,9 +30,12 @@ const output = (prophets) => {
     );
 }
 
-const getProphets = async () => {
+async function getProphets() {
     let response = await fetch(requestURL);
-    prophetsList = await response.json();
-    output(prophetsList);
-    };
-    getProphets();
+    if (response.ok) {
+        let data = await response.json();
+        output(data);
+    } else {
+        throw Error(response.statusText);
+    }
+}
