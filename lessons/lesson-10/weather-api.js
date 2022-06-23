2c5533fd00d0a3a24cdfeee5cfbe0ce3baa6e96d
hmosier <hannah89mosier@byui.edu>
2022-06-23 17:41:07 -0400

    weather api

---
+++ lessons/lesson-10/weather-api.js
@@ -0,0 +1,30 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=2c5b9238c69ab8caead4ac1eaebe1cb0"
+
+async function apiFetch() {
+    const response = await fetch(url);
+    if (response.ok) {
+        const data = await response.json();
+        displayResults(data);
+    } else {
+        throw Error(await response.text());
+    }
+
+  }
+
+  apiFetch();
+
+
+function  displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(1)}</strong>`;
+  
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc.charAt(0).toUpperCase()}${desc.slice(1)}`;
+  }
