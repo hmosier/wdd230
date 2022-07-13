b6e60734a5e2a877d58a4fcae1853334b7dde3dc
hmosier <hannah89mosier@byui.edu>
2022-07-13 16:20:14 -0400

    footer and header mostly formatted

--- final/js/temple-footer.js
+++ final/js/temple-footer.js
@@ -2,3 +2,11 @@ const date = new Date();
 const copyright = "\u00A9";
 
 document.querySelector("#year").textContent = `${copyright} ${date.getFullYear()} .:. Last Updated: ${document.lastModified}`;
+
+function toggleMenu() {
+    document.getElementById("primary").classList.toggle("open");
+    document.getElementById("hamburger").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburger")
+x.onclick = toggleMenu;
